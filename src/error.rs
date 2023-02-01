c82fd905a567b7372e794abdbd0f20c8c191ab92
Julius Liu <juliusl@microsoft.com>
2023-01-31 20:57:30 -0800

    nit. error display formatting

--- src/error.rs
+++ src/error.rs
@@ -82,7 +82,7 @@ impl std::error::Error for Error {}
 
 impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Registry platform error {:?}", self)
+        write!(f, "{:?}", self)
     }
 }
 
