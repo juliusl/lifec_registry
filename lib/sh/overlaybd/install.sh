a78ef78cabf8dce5ca72e8c6ec19fa28268986e9
Julius Liu <juliusl@microsoft.com>
2023-02-17 15:55:19 -0800

    update. try latest .deb (#23)

--- lib/sh/overlaybd/install.sh
+++ lib/sh/overlaybd/install.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Download Overlaybd binaries
-wget https://github.com/containerd/overlaybd/releases/download/v0.6.0/overlaybd-0.6.0-1.x86_64.deb
+wget https://github.com/containerd/overlaybd/releases/download/latest/overlaybd-0.6.0-1.x86_64.deb
 wget https://github.com/containerd/accelerated-container-image/releases/download/v0.6.0/overlaybd-snapshotter_0.6.0_amd64.deb
 sudo apt-get install ./overlaybd-0.6.0-1.x86_64.deb
 sudo apt-get install ./overlaybd-snapshotter_0.6.0_amd64.deb
