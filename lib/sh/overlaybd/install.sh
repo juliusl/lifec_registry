9c176a949cb1112cd7a60708126a9e2691c3e8a2
Julius Liu <juliusl@microsoft.com>
2023-04-17 16:01:14 -0700

    fix. snapshotter downlaod link

--- lib/sh/overlaybd/install.sh
+++ lib/sh/overlaybd/install.sh
@@ -3,11 +3,8 @@ set -e
 
 # Download Overlaybd binaries
 wget https://github.com/containerd/overlaybd/releases/download/latest/overlaybd-0.6.1-0ubuntu1.22.04.x86_64.deb
-wget https://github.com/containerd/accelerated-container-image/releases/download/latest/overlaybd-snapshotter_0.6.0_amd64.deb
+wget https://github.com/containerd/accelerated-container-image/releases/download/latest/overlaybd-snapshotter_0.6.1_amd64.deb
 sudo apt-get install ./overlaybd-0.6.1-0ubuntu1.22.04.x86_64.deb
-sudo apt-get install ./overlaybd-snapshotter_0.6.0_amd64.deb
+sudo apt-get install ./overlaybd-snapshotter_0.6.1_amd64.deb
 rm ./overlaybd-0.6.1-0ubuntu1.22.04.x86_64.deb
-rm ./overlaybd-snapshotter_0.6.0_amd64.deb
-
-# Enable kernel feature
-sudo modprobe target_core_user
+rm ./overlaybd-snapshotter_0.6.1_amd64.deb
