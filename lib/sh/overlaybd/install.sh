7737071d98ae160a3abf7e3234f87ecd60bfe7fc
Ganeshkumar Ashokavardhanan <aganeshkumar@microsoft.com>
2023-05-10 16:21:15 -0700

    fix: remove URL typo

--- lib/sh/overlaybd/install.sh
+++ lib/sh/overlaybd/install.sh
@@ -7,7 +7,7 @@ UBUNTU_RELEASE=22.04
 # Download Overlaybd binaries 
 wget "https://github.com/containerd/overlaybd/releases/download/v${OVERLAYBD_VERSION}/overlaybd-${OVERLAYBD_VERSION}-0ubuntu1.${UBUNTU_RELEASE}.x86_64.deb"
 wget "https://github.com/containerd/accelerated-container-image/releases/download/latest/overlaybd-snapshotter_${OVERLAYBD_SNAPSHOTTER_VERSION}_amd64.deb" # TODO: needs to also suppport ARM64
-sudo apt-get install "./overlaybd-${OVERLAYBD_VERSION}S-0ubuntu1.${UBUNTU_RELEASE}.x86_64.deb"
+sudo apt-get install "./overlaybd-${OVERLAYBD_VERSION}-0ubuntu1.${UBUNTU_RELEASE}.x86_64.deb"
 sudo apt-get install "./overlaybd-snapshotter_${OVERLAYBD_SNAPSHOTTER_VERSION}_amd64.deb"
 rm ./overlaybd-${OVERLAYBD_VERSION}-0ubuntu1.${UBUNTU_RELEASE}.x86_64.deb
 rm ./overlaybd-snapshotter_${OVERLAYBD_SNAPSHOTTER_VERSION}_amd64.deb
