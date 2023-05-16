b0ee3a2dba86b78e940e886b7d0960c7089c945b
Ganeshkumar Ashokavardhanan <aganeshkumar@microsoft.com>
2023-05-16 11:02:51 -0700

    fix: update overlaybd snapshotter URL

--- lib/sh/overlaybd/install.sh
+++ lib/sh/overlaybd/install.sh
@@ -6,7 +6,7 @@ OVERLAYBD_SNAPSHOTTER_VERSION=0.6.2
 UBUNTU_RELEASE=22.04
 # Download Overlaybd binaries 
 wget "https://github.com/containerd/overlaybd/releases/download/v${OVERLAYBD_VERSION}/overlaybd-${OVERLAYBD_VERSION}-0ubuntu1.${UBUNTU_RELEASE}.x86_64.deb"
-wget "https://github.com/containerd/accelerated-container-image/releases/download/latest/overlaybd-snapshotter_${OVERLAYBD_SNAPSHOTTER_VERSION}_amd64.deb" # TODO: needs to also suppport ARM64
+wget "https://github.com/containerd/accelerated-container-image/releases/download/v${OVERLAYBD_SNAPSHOTTER_VERSION}/overlaybd-snapshotter_${OVERLAYBD_SNAPSHOTTER_VERSION}_amd64.deb" # TODO: needs to also suppport ARM64
 sudo apt-get install "./overlaybd-${OVERLAYBD_VERSION}-0ubuntu1.${UBUNTU_RELEASE}.x86_64.deb"
 sudo apt-get install "./overlaybd-snapshotter_${OVERLAYBD_SNAPSHOTTER_VERSION}_amd64.deb"
 rm ./overlaybd-${OVERLAYBD_VERSION}-0ubuntu1.${UBUNTU_RELEASE}.x86_64.deb
